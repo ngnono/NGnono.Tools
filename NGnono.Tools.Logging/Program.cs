74691a78d945d194428dd54b80f6a373334cf7d2
lianghongpeng <lianghongpeng@yintai.com>
2014-06-16 15:26:32 +0800

    add logging

---
+++ NGnono.Tools.Logging/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+//1.
+[assembly: log4net.Config.XmlConfigurator(Watch = true)]
+namespace NGnono.Tools.Logging
+{
+    class Program
+    {
+        
+        static Program()
+        {
+            //2.log4net.Config.XmlConfigurator.ConfigureAndWatch(new FileInfo("log4net.config"));
+        }
+
+        static log4net.ILog _log = log4net.LogManager.GetLogger(typeof(Program));
+        
+        static void Main(string[] args)
+        {
+            _log.Debug("debug");
+            _log.Info("Info");
+            _log.Warn("Warn");
+            _log.Error("Error");
+            _log.Fatal("Fatal");
+
+            Console.ReadLine();
+        }
+    }
+}
